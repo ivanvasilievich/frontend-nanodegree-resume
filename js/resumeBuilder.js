1ada7fea26a2dd1b4ecfc206090d0c9fc32e2554
ivanvasilievich <stanislav.macdonald@XPC-SCO026.xodus.local>
2015-10-06 18:12:01 +0100

    work2

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,3 +2,27 @@ var formattedName = HTMLheaderName.replace("%data%", "Stan Masters");
 var formattedRole = HTMLheaderRole.replace("%data%", "Developer");
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+var skills = ["Very" , "Good" , "At" , "Nothing"];
+var bio = {
+    "name":"Stan",
+    "role":"Developer",
+    "contacts":{
+        "mobile":"12345",
+        "email":"lol@me",
+        "github":"stan.me",
+        "twitter":"shuttit",
+        "location":"Glesgwee"
+    },
+    "welcomeMessage":"welcome to my page!",
+    "skills":skills,
+    "image":"images/fry.jpg"
+};
+$("#main").append(bio.name);
+var work = {
+    "name":"Exodus",
+    "post":"enigineer"
+};
+var education = {
+    "name":"University",
+    "type":"Masters"
+};
